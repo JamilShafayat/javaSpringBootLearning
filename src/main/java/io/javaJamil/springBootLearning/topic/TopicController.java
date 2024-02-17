a564f4a6e0005badfd74a4c11c0f738f2b6cb457
jamilshafayat <jamilshafayat8@gmail.com>
2024-02-17 08:22:46 +0600

    Creation testing route and apis as the name of topic

--- src/main/java/io/javaJamil/springBootLearning/topic/TopicController.java
+++ src/main/java/io/javaJamil/springBootLearning/topic/TopicController.java
@@ -3,11 +3,18 @@ package io.javaJamil.springBootLearning.topic;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class TopicController {
 
     @RequestMapping("/topics")
-    public String getAllTopics() {
-        return "All topics";
+    public List<Topic> getAllTopics() {
+        return Arrays.asList(
+                new Topic("Spring", "Java framework", "Java framework description"),
+                new Topic("Nest", "Node framework", "Node framework description"),
+                new Topic("Next", "React framework", "React framework description")
+        );
     }
 }
